caa108d65d78f42389779c7846b756fa53673921
Kela <seba.gallardo.huerta@gmail.com>
2025-09-12 17:25:55 -0300

    FIX - mount CLP suma total

--- src/views/Finanzas.tsx
+++ src/views/Finanzas.tsx
@@ -203,19 +203,19 @@ export const Finanzas = () => {
                     <td className="px-6 py-4">${finanza.monto}</td>
                     <td className="px-6 py-4">{finanza.fecha}</td>
                     <td className="px-6 py-4">
-                      <span className="bg-blue-100 text-blue-800 text-xs font-medium px-2.5 py-0.5 rounded">
+                      <span className="bg-blue-100 text-blue-800 text-xs font-medium px-2.5 py-0.5 rounded flex items-center justify-center mb-3">
                         {finanza.categoria}
                       </span>
                     </td>
-                    <td className="px-6 py-4">
+                    <td className="px-6 py-4 flex items-center justify-center">
                       <button
-                        className="text-blue-600 hover:text-blue-900 mr-2"
+                        className="text-blue-600 hover:text-blue-900 mr-2 bg-blue-100 rounded w-20"
                         onClick={() => handleEdit(finanza.id)}
                       >
                         Editar
                       </button>
                       <button
-                        className="text-red-600 hover:text-red-900"
+                        className="text-red-600 hover:text-red-900  bg-red-100 rounded w-20"
                         onClick={() => handleDelete(finanza.id)}
                       >
                         Eliminar
